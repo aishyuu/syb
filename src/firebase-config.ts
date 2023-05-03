5a6bedc6920182506721d210718fb04e830e1aea
Ivan Matias <ivanmatias1331@gmail.com>
2023-05-03 12:40:39 -0700

    added connection to firebase

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,10 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from '@firebase/firestore'
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-//   insert firebase configuration here!
+  apiKey: "AIzaSyBB21sd_eZftHdx_ymzD8SjHP5AocYPsnI",
+
+  authDomain: "syb-database.firebaseapp.com",
+
+  databaseURL: "https://syb-database-default-rtdb.firebaseio.com",
+
+  projectId: "syb-database",
+
+  storageBucket: "syb-database.appspot.com",
+
+  messagingSenderId: "357903313428",
+
+  appId: "1:357903313428:web:11a8a490191f92bcabd501",
 };
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app)
+export const db = getFirestore(app);
